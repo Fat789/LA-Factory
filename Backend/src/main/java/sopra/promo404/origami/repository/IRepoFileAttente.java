ffc95e1ad6c4712a42e04043334b4c8d962c05ad
Fat <baptiste.6@hotmail.com>
2018-09-20 11:42:42 +0200

    Mise à jour

---
+++ Backend/src/main/java/sopra/promo404/origami/repository/IRepoFileAttente.java
@@ -0,0 +1,17 @@
+package sopra.promo404.origami.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sopra.promo404.origami.model.FileAttente;
+
+public interface IRepoFileAttente extends JpaRepository<FileAttente, Long>{
+
+	@Query("Select distinct f from FileAttente f left join fetch f.patient left join fetch f.medecin where f.id =:id")
+	FileAttente findFileAttenteByIdWithPatient(@Param("id")Long id);
+	
+	@Query("Select distinct f from FileAttente f left join fetch f.secretaire")
+	List<FileAttente>findAllFileAttente();
+}
---
+++ Backend/src/main/java/sopra/promo404/origami/repository/IRepoMedecin.java
@@ -0,0 +1,18 @@
+package sopra.promo404.origami.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sopra.promo404.origami.model.Medecin;
+
+public interface IRepoMedecin extends JpaRepository<Medecin, Long> {
+
+	@Query("select distinct m from Medecin m left join fetch m.specialites s where m.id= :id")
+	Medecin findMedecinByIdWithSpecialite(@Param("id") Long id);
+	
+	@Query("select m from Medecin m left outer join fetch m.salle")
+	List<Medecin> findAllMedecin();
+}
